040a7c0bf687247bace699d3357a1f99317ff0f6
cGoyal69 <127645215+cGoyal69@users.noreply.github.com>
2025-04-06 20:27:06 +0530

    database change

--- app/core/database.py
+++ app/core/database.py
@@ -6,7 +6,7 @@ from app.core.config import settings
 
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql+psycopg2://postgresql:1l8TBIbausK6TwyOBP1jfGOZT4EXHjTI@localhost:5432/dbname_cektL")
+engine = create_engine("postgresql://postgres:lWDtnVMWwwdSQFtpVZtWJNnxBeeLDLeH@soundverse-db.railway.internal:5432/railway")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
